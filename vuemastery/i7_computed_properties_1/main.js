60ba989e5fa1bd33689d66c1b5bd6d79058e5f4f
sanan1211 <kinglso2@naver.com>
2019-11-25 21:51:29 +0900

    add computed properties add components

---
+++ vuemastery/i7_computed_properties_1/main.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        brand: 'Vue Mastery',
+        product: 'Socks',
+        image: './assets/vmSocks-green.jpg',
+        inStock: false,
+        details: ["80% cotton", "20% polyster", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        }
+    }
+})
